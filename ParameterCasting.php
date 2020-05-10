18cf5bebea0e50f30f71e1c7bc0de580e00a1743
taufanbasri <taufanprasetyobasri@gmail.com>
2020-05-10 12:44:58 +0700

    Parameter Casting

---
+++ ParameterCasting.php
@@ -0,0 +1,28 @@
+<?php
+
+class ParameterCasting
+{
+    public function __construct(array $array) {
+        
+    }
+
+    public function stringCast(string $string)
+    {
+        
+    }
+
+    public function intCast(int $int)
+    {
+        
+    }
+
+    public function floatCast(float $float)
+    {
+        
+    }
+
+    public function booleanCast(bool $bool)
+    {
+        
+    }
+}
